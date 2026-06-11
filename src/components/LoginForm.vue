b0b4740825463e90b50c8c7e18cd66172e787bff
Gustavo000Yagame <geraisusos25@gmail.com>
2026-06-11 18:37:21 -0300

    atualizacao formulario

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -4,7 +4,7 @@
     justify-content: flex-end; 
     align-items: center;      
     min-height: 100vh;        
-    padding-right: 50px;    
+    padding-right: 30px;    
     background-color: #f4f4f7; 
   }
 
