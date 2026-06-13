8a1ec770390d9329a61005373adcf02334f07637
Gustavo000Yagame <geraisusos25@gmail.com>
2026-06-12 22:28:33 -0300

    jngd mais para a esquerda

--- src/components/Feedback.vue
+++ src/components/Feedback.vue
@@ -290,5 +290,5 @@
     </div>
   </div>
 </template>
-<div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto px-3 py-7">
+<div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto px-7 py-7">
 </div>
